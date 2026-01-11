72d8e31896bc3868bc2f0e0ff8a10fe5b68bf109
Miriasme <johndoe@example.com>
2026-01-11 20:30:11 +0300

    [insert data to player and team]

---
+++ k3_td2/src/main/resources/archetype-resources/src/main/java/database/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO TEAM (
+                  id,name,continent
+) values (
+          1,'Real Madrid CF','EUROPA'
+         ),
+      (2,'FC barcelona','EUROPA'),
+      (3,'Atlético de Madrid','EUROPA'),
+      (4,'Al Ahly SC','AFRICA'),
+      (5,'Inter Miami CF','AMERICA');
+
+INSERT INTO PLAYER (
+    id,name,age,position,id_team
+) values (
+             1,'Thibaut Courtois',32,'GK',1
+         ),
+         (2,'Dani Carvajal',33,'DEF',1),
+         (3,'Jude Bellingham',21,'MIDF',1),
+         (4,'Robert Lewandoski',36,'STR',2),
+         (5,'Antoine Griezman',33,'STR',3);
+
