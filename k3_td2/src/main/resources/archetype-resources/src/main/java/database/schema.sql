1b2aa12fa942419fccb9b056c4b00d64e5f6a15d
Miriasme <johndoe@example.com>
2026-01-11 20:17:58 +0300

    [create table teama and player ]

---
+++ k3_td2/src/main/resources/archetype-resources/src/main/java/database/schema.sql
@@ -0,0 +1,21 @@
+CREATE TYPE position_enum
+    AS
+    ENUM('GK', 'DEF', 'MIDF', 'STR');
+CREATE TYPE continent_enum
+    AS
+    ENUM('AFRICA', 'EUROPA', 'ASIA',
+'AMERICA');
+CREATE TABLE TEAM(
+                       id int,
+                       name varchar(150),
+                       continent continent_enum NOT NULL
+
+);
+
+CREATE TABLE PLAYER(
+    id int,
+    name varchar(150),
+    age int,
+    position position_enum NOT NULL ,
+    id_team int
+);
